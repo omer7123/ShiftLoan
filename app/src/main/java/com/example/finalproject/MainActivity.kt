f23daf32428945e5c7f68cd8e2594f2739fa73e0
Фомин Илья Сергеевич <ilyafomin125@gmail.com>
2024-06-12 19:04:22 +0700

    Создал проект, добавил все цвета для тем, добавил nav_graph и bottomNavigation

---
+++ app/src/main/java/com/example/finalproject/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.finalproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initNavControllerWithBottomNav()
+        initDestination()
+    }
+
+    private fun initDestination() {
+        navController.addOnDestinationChangedListener { navController, destination, arguments ->
+
+//            if (destination.id == R.id.registerFragment ||
+//                destination.id == R.id.authFragment
+//
+//            ) {
+//                binding.navView.visibility = View.GONE
+//            } else {
+//                binding.navView.visibility = View.VISIBLE
+//            }
+        }
+    }
+
+    private fun initNavControllerWithBottomNav() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
+    }
+}
