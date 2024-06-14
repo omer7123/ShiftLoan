5884aaad102d5a5cea2c5f9336b16367f2f44c63
Фомин Илья Сергеевич <ilyafomin125@gmail.com>
2024-06-15 05:19:15 +0700

    Реализовал сохранение данных для автоматической авторизации

--- app/src/main/java/com/example/finalproject/presentation/authorizationFragment/AuthorizationViewModel.kt
+++ app/src/main/java/com/example/finalproject/presentation/authorizationFragment/AuthorizationViewModel.kt
@@ -7,30 +7,42 @@ import androidx.lifecycle.viewModelScope
 import com.example.finalproject.core.Resource
 import com.example.finalproject.domain.entity.AuthEntity
 import com.example.finalproject.domain.useCase.LoginUseCase
+import com.example.finalproject.domain.useCase.SaveSharedPrefAuthUseCase
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class AuthorizationViewModel @Inject constructor(
-    private val loginUseCase: LoginUseCase
+    private val loginUseCase: LoginUseCase,
+    private val saveSharedPrefAuthUseCase: SaveSharedPrefAuthUseCase
 ) : ViewModel() {
 
     private val _screenState: MutableLiveData<AuthorizationScreenState> =
         MutableLiveData(AuthorizationScreenState.Initial)
     val screenState: LiveData<AuthorizationScreenState> = _screenState
 
+    val handler = CoroutineExceptionHandler { _, error ->
+        _screenState.value = AuthorizationScreenState.Error("Произошла ошибка, попробуйте еще раз")
+    }
+
     fun auth(auth: AuthEntity) {
         if (validationData(auth.name, auth.password)) {
-            viewModelScope.launch {
+            viewModelScope.launch(handler) {
                 _screenState.value = AuthorizationScreenState.Loading
                 when (val result = loginUseCase(auth)) {
                     is Resource.Error -> _screenState.value =
                         AuthorizationScreenState.Error(result.msg.toString())
 
                     Resource.Loading -> _screenState.value = AuthorizationScreenState.Loading
-                    is Resource.Success -> _screenState.value = AuthorizationScreenState.Success
+                    is Resource.Success -> renderSuccess(auth)
                 }
             }
         }
+    }
+
+    private suspend fun renderSuccess(auth: AuthEntity) {
+        saveSharedPrefAuthUseCase(auth)
+        _screenState.value = AuthorizationScreenState.Success
     }
 
     private fun validationData(name: String, password: String): Boolean {
--- app/src/main/java/com/example/finalproject/presentation/registrationFragment/RegistrationViewModel.kt
+++ app/src/main/java/com/example/finalproject/presentation/registrationFragment/RegistrationViewModel.kt
@@ -6,17 +6,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.finalproject.core.Resource
 import com.example.finalproject.domain.entity.AuthEntity
+import com.example.finalproject.domain.useCase.LoginUseCase
 import com.example.finalproject.domain.useCase.RegistrationUseCase
+import com.example.finalproject.domain.useCase.SaveSharedPrefAuthUseCase
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class RegistrationViewModel @Inject constructor(
-    private val registrationUseCase: RegistrationUseCase
+    private val registrationUseCase: RegistrationUseCase,
+    private val loginUseCase: LoginUseCase,
+    private val saveSharedPrefAuthUseCase: SaveSharedPrefAuthUseCase,
 ) : ViewModel() {
 
     private val _screenState: MutableLiveData<RegistrationScreenState> =
         MutableLiveData(RegistrationScreenState.Initial)
     val screenState: LiveData<RegistrationScreenState> = _screenState
+
 
     fun register(name: String, password: String, confirmPassword: String) {
 
@@ -29,13 +34,26 @@ class RegistrationViewModel @Inject constructor(
                         RegistrationScreenState.Error(result.msg.toString())
 
                     Resource.Loading -> _screenState.value = RegistrationScreenState.Loading
-                    is Resource.Success -> _screenState.value = RegistrationScreenState.Success
+                    is Resource.Success -> authorization(auth)
                 }
             }
         }
     }
 
-    private fun validationData(name: String, password: String, confirmPassword: String): Boolean{
+    private suspend fun authorization(auth: AuthEntity) {
+        when (val result = loginUseCase(auth)) {
+            is Resource.Error -> _screenState.value =
+                RegistrationScreenState.Error(result.msg.toString())
+
+            Resource.Loading -> _screenState.value = RegistrationScreenState.Loading
+            is Resource.Success -> {
+                saveSharedPrefAuthUseCase(auth)
+                _screenState.value = RegistrationScreenState.Success
+            }
+        }
+    }
+
+    private fun validationData(name: String, password: String, confirmPassword: String): Boolean {
         val nameError = if (name.isEmpty() || !name.matches(Regex("^[a-zA-Z0-9]*$"))) {
             if (name.isEmpty()) "Поле не может быть пустым" else "Только цифры и латинские буквы"
         } else null
